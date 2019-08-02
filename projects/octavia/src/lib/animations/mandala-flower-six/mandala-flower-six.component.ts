219a8e360cda913a0381045c785063ac56c40e14
Melissa Mepham <melissa.mepham@trademe.co.nz>
2019-08-02 21:12:25 +1200

    added the colors in and had to refactor because the animation stopped working - will need to refactor the mandala a little to make it look nice not animated

--- projects/octavia/src/lib/animations/mandala-flower-six/mandala-flower-six.component.ts
+++ projects/octavia/src/lib/animations/mandala-flower-six/mandala-flower-six.component.ts
@@ -3,7 +3,6 @@ import {debounceTime, takeUntil} from 'rxjs/operators';
 import {BehaviorSubject, Subject} from 'rxjs';
 import * as sketch from 'p5';
 
-import {MandalaFlowerSixColorEnum} from "./mandala-flower-six.enum";
 import * as COLOR_DATA from "../../../assets/colors.json";
 
 
@@ -18,7 +17,6 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
   private _canvasObservable$ = new BehaviorSubject(this._canvasObservable);
   private _destroyed$ = new Subject<void>();
   private _sketch;
-  private _c = MandalaFlowerSixColorEnum;
   private _p;
   private _s;
   private _h;
@@ -44,7 +42,7 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
     )
   }
 
-  @Input() isAnimated: boolean;
+  @Input() isAnimated = true;
 
   @Input() primaryColor = "green";
   @Input() secondaryColor = "blue";
@@ -72,7 +70,7 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
     this.canvasDocument = document.querySelector("canvas")
         ? document.querySelector("canvas")
         : document.querySelector("div");
-    this.checkIsAnimated();
+    // this.checkIsAnimated();
   }
 
   private destroyCanvas(): void {
@@ -194,24 +192,24 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
       // Outer flower shape
       p.push();
         p.translate(p.center.x, p.center.y);
-        p.fill(p.color(this._p['100']));
-        p.stroke(p.color(this._c.oceanBlue400));
-        for (let i = 0; i < 12; i++) {
+        p.fill(p.color(this._p['600']));
+        p.stroke(p.color(this._p['600']));
+        p.rotate(p.radians(p.frameCount * 60) / -3);
+      for (let i = 0; i < 12; i++) {
           p.beginShape();
           p.curve(curve, curve, -curvePointY, -curvePointX, curvePointY, -curvePointX, -curve, curve);
           p.rotate(30)
           p.endShape();
         }
-        p.rotate(p.radians(p.frameCount * 60) / -3);
       p.pop();
 
       p.push();
         p.noStroke();
-        p.fill(p.color(this._c.oceanBlue200));
+        p.fill(p.color(this._p["100"]));
         p.translate(p.center.x, p.center.y);
-        p.circle(0, 0, biggestCircle);
         p.scale((p.sin(p.frameCount / 5) * 1) + .2);
         p.rotate(p.radians(p.frameCount * 60) / 3);
+        p.circle(0, 0, biggestCircle);
       p.pop();
 
       // Curved triangles
@@ -221,7 +219,9 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
         p.noStroke();
         for (let i = 0; i < 24; i++) {
           p.beginShape();
-          p.fill(this._c.oceanGreen400);
+          p.fill(this._p["500"]);
+          p.scale((p.sin(p.frameCount / 5) * 1) + .2);
+          p.rotate(p.radians(p.frameCount * 60) / 3);
           p.vertex(-smallCurvedTriangleStartPoint, -smallCurvedTriangleHeight);
           p.bezierVertex(-smallCurvedTriangleStartPoint, -smallCurvedTriangleHeight, -smallCurvedTrianglesmallestControlPoint, -smallCurvedTriangleHeight, 0, -smallCurvedTriangleHighestHeight);
           p.bezierVertex(0, -smallCurvedTriangleHighestHeight, smallCurvedTrianglesmallestControlPoint, -smallCurvedTriangleHeight, smallCurvedTriangleStartPoint, -smallCurvedTriangleHeight);
@@ -229,90 +229,86 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
           p.endShape(p.CLOSE);
           p.rotate(15);
         }
-        p.scale((p.sin(p.frameCount / 5) * 1) + .2);
-        p.rotate(p.radians(p.frameCount * 60) / 3);
       p.pop();
 
       // The circles in the triangles
       p.push();
         p.noStroke();
         p.translate(p.center.x, p.center.y);
-        p.fill(this._c.yellowGreen200);
+        p.fill(this._h["100"]);
+        p.scale((p.sin(p.frameCount) * 1) + .5);
+        p.rotate(p.radians(p.frameCount * 40));
         for (let i = 0; i < 24; i++) {
           p.circle(0, -smallCurvedTriangleHeight, circleInCurvedTriangles);
           p.rotate(15);
         }
-        p.scale((p.sin(p.frameCount) * 1) + .5);
-        p.rotate(p.radians(p.frameCount * 40));
       p.pop();
 
       // big blue circle
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-        p.fill(p.color(this._c.green100));
-        p.circle(0, 0, nearlyBiggestCircle);
+        p.fill(p.color(this._s["500"]));
         p.scale((p.sin(p.frameCount) * 1) + .2);
+        p.circle(0, 0, nearlyBiggestCircle);
       p.pop();
 
       // big blue circle
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-      p.fill(p.color(this._c.oceanBlue300));
-      p.circle(0, 0, bigCircleC);
+      p.fill(p.color(this._s["300"]));
       p.scale((p.sin(p.frameCount) * 1) + .2);
+      p.circle(0, 0, bigCircleC);
       p.pop();
 
       // long skinny lots of  triangles
       p.push();
-      p.translate(p.center.x, p.center.y);
-      p.noStroke();
-          p.fill(this._c.hotPink200);
+        p.translate(p.center.x, p.center.y);
+        p.noStroke();
+        p.fill(this._h["200"]);
+        p.rotate(p.radians(p.frameCount * 30));
+        p.scale((p.sin(p.frameCount / 2) * 1.6) + .2);
           for (let i = 0; i < 12; i++) {
             p.triangle(-trianglex1, triangley1, -trianglex1, -triangley1, -trianglex3, 0);
             p.rotate(30);
           }
-
         // water droplets
-          p.noStroke();
-          p.beginShape();
-            for (let i = 0; i < 12; i++) {
-              p.fill(this._c.oceanBlue200);
-              p.bezier(0, -waterDropletY1, -waterDropletX2, -waterDropletY2, waterDropletX2, -waterDropletY2, 0, -waterDropletY1);
-              p.fill(this._c.hotPink200);
-              p.circle(0, -waterDropletCircleY, waterDropletCircleC);
-              p.rotate(30);
-            };
-          p.endShape();
-        p.rotate(p.radians(p.frameCount * 30));
-        p.scale((p.sin(p.frameCount / 2) * 1.6) + .2);
+        p.noStroke();
+        p.beginShape();
+          for (let i = 0; i < 12; i++) {
+            p.fill(this._s["600"]);
+            p.bezier(0, -waterDropletY1, -waterDropletX2, -waterDropletY2, waterDropletX2, -waterDropletY2, 0, -waterDropletY1);
+            p.fill(this._s["200"]);
+            p.circle(0, -waterDropletCircleY, waterDropletCircleC);
+            p.rotate(30);
+          };
+        p.endShape();
       p.pop();
 
       p.push();
         p.translate(p.center.x, p.center.y);
-        p.fill(this._c.oceanBlue300);
+        p.fill(this._p["200"]);
         p.noStroke();
         p.push();
-          p.beginShape();
-          // p.rotate(30);
-          p.rotate(p.radians(p.frameCount * -40));
-          for (let i = 0; i < 6; i++) {
-            p.rotate(60);
-            p.bezier(0, -waterDropletLittleY2, -waterDropletLittleX2, -waterDropletLittleY1, waterDropletLittleX2, -waterDropletLittleY1, 0, -waterDropletLittleY2);
-          };
+        p.scale((p.sin(p.frameCount / 5) * 1.6) + .2);
+        p.beginShape();
+        p.rotate(p.radians(p.frameCount * -40));
+        for (let i = 0; i < 6; i++) {
+          p.rotate(60);
+          p.bezier(0, -waterDropletLittleY2, -waterDropletLittleX2, -waterDropletLittleY1, waterDropletLittleX2, -waterDropletLittleY1, 0, -waterDropletLittleY2);
+        };
           p.endShape();
-          p.scale((p.sin(p.frameCount / 5) * 1.6) + .2);
         p.pop();
 
         p.push();
           p.beginShape();
+          p.rotate(p.radians(p.frameCount * 40));
           for (let i = 0; i < 6; i++) {
             p.rotate(60);
             p.bezier(0, -waterDropletLittleY2, -waterDropletLittleX2, -waterDropletLittleY1, waterDropletLittleX2, -waterDropletLittleY1, 0, -waterDropletLittleY2);
           };
           p.endShape();
-          p.rotate(p.radians(p.frameCount * 40));
         p.pop();
       p.pop();
 
@@ -320,39 +316,41 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
+        p.rotate(p.radians(p.frameCount * -20));
         for (let i = 0; i < 12; i++) {
-            p.fill(this._c.oceanBlue400);
+            p.fill(this._p["500"]);
             p.circle(outerTripleCirclesBigX, 0, outerTripleCirclesBigC);
-            p.fill(this._c.oceanBlue300);
+            p.fill(this._p["300"]);
             p.circle(outerTripleCirclesMidSmlX, 0, outerTripleCirlcesMidC);
-            p.fill(this._c.oceanBlue200);
+            p.fill(this._p["100"]);
             p.circle(outerTripleCirclesMidSmlX, 0, outerTripleCirclesSmlC);
             p.rotate(30);
           };
-        p.rotate(p.radians(p.frameCount * -20));
       p.pop();
 
       // Ctriangle
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-        p.fill(this._c.oceanGreen400);
+        p.fill(this._p["400"]);
+        p.rotate(p.radians(p.frameCount / 4) * 50);
         for (let i = 0; i < 6; i++) {
           p.triangle(Ctrianglex1, -Ctriangley1, -Ctrianglex1, -Ctriangley1, 0, -Ctriangley2)
           p.rotate(60);
         }
-        p.rotate(p.radians(p.frameCount / 4) * 50);
       p.pop();
 
       // flowerA
       p.push();
       p.translate(p.center.x, p.center.y);
-      p.fill(p.color(this._c.limeGreen300));
+      p.fill(p.color(this._s["700"]));
       p.push();
           p.noStroke();
           p.ellipse(0, 0, flowerCircle, flowerCircle);
         p.pop();
         p.push();
+          p.scale((p.sin(p.frameCount / 2) * 1.6) + .2);
+          p.rotate(p.radians(p.frameCount * -50));
           p.noStroke();
           for (let i = 0; i < 6; i++) {
             p.beginShape();
@@ -363,67 +361,65 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
             p.rotate(60);
           }
         p.pop();
-        p.scale((p.sin(p.frameCount / 2) * 1.6) + .2);
-        p.rotate(p.radians(p.frameCount * -50));
       p.pop();
 
       // outer triple circles
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
+        p.scale((p.sin(p.frameCount / 2) * .5) + 1);
+        p.rotate(p.radians(p.frameCount / 4) * -50);
         p.push();
         p.scale((p.sin(p.frameCount / 5) * 1.5) + 1);
         for (let i = 0; i < 12; i++) {
-          p.fill(this._c.oceanBlue400);
+          p.fill(this._p["400"]);
           p.circle(outerTripleCirclesBigX, 0, outerTripleCirclesBigC / 4);
           p.rotate(30);
         };
       p.pop();
       p.push();
         for (let i = 0; i < 12; i++) {
-          p.fill(this._c.oceanBlue300);
+          p.fill(this._p["500"]);
           p.circle(outerTripleCirclesMidSmlX, 0, outerTripleCirlcesMidC / 4);
-          p.fill(this._c.oceanBlue200);
+          p.fill(this._p["300"]);
           p.circle(outerTripleCirclesMidSmlX, 0, outerTripleCirclesSmlC / 4);
           p.rotate(30);
         };
         p.pop();
-      p.scale((p.sin(p.frameCount / 2) * .5) + 1);
-      p.rotate(p.radians(p.frameCount / 4) * -50);
       p.pop();
 
       // Ctriangle
       p.push();
       p.translate(p.center.x, p.center.y);
       p.noStroke();
-      p.fill(this._c.oceanGreen400);
+      p.fill(this._p["600"]);
+      p.rotate(p.radians(p.frameCount / 4) * 50);
       for (let i = 0; i < 6; i++) {
         p.triangle(Ctrianglex1, -Ctriangley1, -Ctrianglex1, -Ctriangley1, 0, -Ctriangley2)
         p.rotate(60);
       }
-      p.rotate(p.radians(p.frameCount / 4) * 50);
       p.pop();
 
       // ECircle
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-        p.fill(p.color(this._c.green200));
-        p.ellipse(0, 0, ECircleHW, ECircleHW)
+        p.fill(p.color(this._p["200"]));
+        p.ellipse(0, 0, ECircleHW, ECircleHW);
       p.pop();
 
       // CCircle
       p.push();
         p.translate(p.center.x, p.center.y);
-        for (let i = 0; i < 6; i++) {
-          p.noStroke();
-          p.fill(p.color(this._c.oceanGreen400));
-          p.ellipse(0, -CCircleY, DCircleWH, DCircleWH);
-          p.fill(p.color(this._c.peach100));
-          p.ellipse(0, -CCircleY, CCircleWH, CCircleWH);
-          p.rotate(60);
-        }
         p.scale((p.sin(p.frameCount / 2) * 1.3) + .2);
+        for (let i = 0; i < 6; i++) {
+            p.noStroke();
+            p.fill(p.color(this._s["400"]));
+            p.ellipse(0, -CCircleY, DCircleWH, DCircleWH);
+            p.fill(p.color(this._s["300"]));
+            p.ellipse(0, -CCircleY, CCircleWH, CCircleWH);
+            p.rotate(60);
+        }
       p.pop();
 
       // Bcircles
@@ -431,21 +427,21 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
         p.translate(p.center.x, p.center.y);
         p.noStroke();
         p.push();
-        p.fill(this._c.oceanBlue400);
+        p.fill(this._p["500"]);
         p.scale(1.2);
-        for (let i = 0; i < 6; i++) {
-          p.ellipse(-BcircleX, 0, BcircleSize, BcircleSize);
-          p.rotate(60);
-        }
         p.rotate(p.radians(p.frameCount * -50));
+        for (let i = 0; i < 6; i++) {
+            p.ellipse(-BcircleX, 0, BcircleSize, BcircleSize);
+            p.rotate(60);
+          }
       p.pop();
       p.push();
-        p.fill(151, 242, 255, 150);
+      p.rotate(p.radians(p.frameCount / 3) * 50);
+      p.fill(this._p["200"]);
         for (let i = 0; i < 6; i++) {
           p.ellipse(-BcircleX, 0, BcircleSize, BcircleSize);
           p.rotate(60);
         }
-        p.rotate(p.radians(p.frameCount / 3) * 50);
         p.pop();
       p.pop();
 
@@ -453,40 +449,40 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-        p.fill(this._c.green200);
+        p.fill(this._s["200"]);
+        p.rotate(p.radians(p.frameCount / 4) * 50);
+        p.scale((p.sin(p.frameCount / 1) * 1.3) + .5);
         for (let i = 0; i < 6; i++) {
             p.triangle(Atrianglex1, -Atriangley1, -Atrianglex1, -Atriangley1, 0, -Atriangley2)
             p.rotate(60);
         }
-        p.fill(this._c.oceanBlue400);
+        p.fill(this._s["400"]);
         for (let i = 0; i < 6; i++) {
           p.triangle(Atrianglex1, -Atriangley1, -Atrianglex1, -Atriangley1, 0, -Btriangley1)
           p.rotate(60);
         }
-        p.rotate(p.radians(p.frameCount / 4) * 50);
-        p.scale((p.sin(p.frameCount / 1) * 1.3) + .5);
       p.pop();
 
       // Acircles
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-        p.fill(this._c.hotPink200);
+        p.fill(this._h["300"]);
+        p.rotate(p.radians(p.frameCount / 1) * -50);
+        p.scale((p.sin(p.frameCount / 2) * 1.3) + .5);
         for (let i = 0; i < 6; i++) {
           p.ellipse(-AcircleX, 0, AcircleSize, AcircleSize);
           p.rotate(60);
         }
-        p.rotate(p.radians(p.frameCount / 1) * -50);
-        p.scale((p.sin(p.frameCount / 2) * 1.3) + .5);
       p.pop();
 
       // circle framing the flower of life
       p.push();
         p.translate(p.center.x, p.center.y);
         p.noStroke();
-        p.fill(255, 255, 255, 200);
-        p.ellipse(0, 0, petal * 2 , petal * 2);
+        p.fill(this._p["200"]);
         p.scale((p.sin(p.frameCount / 3) * 1.3) + .5);
+        p.ellipse(0, 0, petal * 2 , petal * 2);
       p.pop();
 
       // flower of life
@@ -494,13 +490,14 @@ export class MandalaFlowerSixComponent implements OnInit, OnDestroy, OnChanges {
         p.translate(p.center.x, p.center.y);
         p.strokeWeight(0.1);
         p.stroke(255, 255, 255, 0);
-        p.fill(p.color(this._c.limeGreen300));
+        p.fill(p.color(this._p["600"]));
+        p.scale((p.sin(p.frameCount / 4) * .5) + .5);
+        p.rotate(p.radians(p.frameCount / 4) * -50);
         for (let i = 0; i < 6; i++) {
           p.curve(petal, 0, 0, 0, 0, petal, petal, petal);
           p.curve(-petal, 0, 0, 0, 0, petal, -petal, petal);
           p.rotate(60);
         }
-        p.scale((p.sin(p.frameCount / 4) * .5) + .5);
         p.rotate(p.radians(p.frameCount / 4) * -50);
       p.pop();
 
