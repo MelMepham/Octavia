c5e95021e11ebe2c116f682547bf8c83d9c92fd8
melmepham <m.a.mepham@gmail.com>
2019-08-03 18:01:29 +1200

    added svg in

--- projects/web/src/app/shell/header/header.module.ts
+++ projects/web/src/app/shell/header/header.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { ButtonModule } from '../../../../../octavia/src/public-api'
-
+import {LogoComponent} from "../../../assets/logo-svg/logo.component";
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    LogoComponent
+  ],
   exports: [
     HeaderComponent
   ],
