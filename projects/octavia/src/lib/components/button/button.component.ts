850c19a44ebe728f4bb0adbf5f7a5c7abf1a7fdf
melmepham <m.a.mepham@gmail.com>
2019-08-03 15:47:19 +1200

    animated button

--- projects/octavia/src/lib/components/button/button.component.ts
+++ projects/octavia/src/lib/components/button/button.component.ts
@@ -9,6 +9,7 @@ import {Component, HostBinding, OnInit, ViewEncapsulation} from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   @HostBinding('class.oct-button') public isPureButton = true;
+
   constructor() { }
 
   ngOnInit() {
