5b9d43cbf8d06bf849bf2ae25a2fb8ef3bb31790
Melissa Mepham <melissa.mepham@trademe.co.nz>
2019-08-28 09:44:59 +1200

    hey

--- projects/web/src/environments/environment.prod.ts
+++ projects/web/src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDhIf5bhlJRhkAZT5obwB5rJRtuS9ioGTA",
-    authDomain: "melissa-mepham-web.firebaseapp.com",
-    databaseURL: "https://melissa-mepham-web.firebaseio.com",
-    projectId: "melissa-mepham-web",
-    storageBucket: "",
-    messagingSenderId: "852222649405",
-    appId: "1:852222649405:web:8479fc863c60ddee"
-  }
+  production: true
 };
--- projects/web/src/environments/environment.ts
+++ projects/web/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyDhIf5bhlJRhkAZT5obwB5rJRtuS9ioGTA",
-    authDomain: "melissa-mepham-web.firebaseapp.com",
-    databaseURL: "https://melissa-mepham-web.firebaseio.com",
-    projectId: "melissa-mepham-web",
-    storageBucket: "",
-    messagingSenderId: "852222649405",
-    appId: "1:852222649405:web:8479fc863c60ddee"
-  }
+  production: false
 };
 
 /*
