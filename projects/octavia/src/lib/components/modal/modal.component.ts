2913e7a573f78d97079181ebfef8cf16f1d0fa25
melmepham <m.a.mepham@gmail.com>
2019-10-14 22:07:42 +1300

    have got the modal working

--- projects/octavia/src/lib/components/modal/modal.component.ts
+++ projects/octavia/src/lib/components/modal/modal.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
 
 @Component({
   selector: 'Oct-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public isOpen = false;
+  private _element: any;
+  @Input() id: string;
 
-  ngOnInit() {
+  public constructor(
+      private _modalService: ModalService,
+      private _el: ElementRef
+  ) {
+    this._element = _el.nativeElement;
   }
+
+  public ngOnInit (): void {
+    let modal = this;
+    console.log(modal);
+
+    if(!this.id) {
+      console.log("element has to have an id");
+      return;
+    }
+
+    document.body.append(this._element);
+
+    this._modalService.add(this);
+  }
+
+  public ngOnDestroy (): void {
+    this._modalService.remove(this.id);
+    this._element.remove();
+  }
+
+  public open() {
+    this.isOpen = true;
+  }
+  public close() {
+    this.isOpen = false;
+  }
+
 
 }
